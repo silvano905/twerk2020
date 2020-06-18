4ddf43676b66742495d654037806fcbd62eb1d18
silvano valdez <silvanovaldez90@gmail.com>
2020-06-18 11:14:47 -0500

    heree

--- teex/settings.py
+++ teex/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ['MY_KEY_TWERK']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ruiz90.pythonanywhere.com', '127.0.0.1']
 
 
 # Application definition
