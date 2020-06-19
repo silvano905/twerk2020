b8d75da2b7417695018eb7ee82ecd43f1db39c1f
silvano valdez <silvanovaldez90@gmail.com>
2020-06-18 22:19:02 -0500

    silvano2

--- teex/settings.py
+++ teex/settings.py
@@ -84,8 +84,12 @@ WSGI_APPLICATION = 'teex.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'djangodatabase',
+        'USER': 'silvano',
+        'PASSWORD': 'Realmadrid90',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
